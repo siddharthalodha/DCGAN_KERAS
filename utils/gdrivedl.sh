5bb8a81afd7f59662401878d073fa43a94b5d0d5
Siddhartha <siddhartha.2608@gmail.com>
2018-06-13 13:14:42 +0530

    Initial commit

---
+++ utils/gdrivedl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ggID='1cUBPxqU-9Y6f_OAfPwdRmPg7ruZ2sfdk'  
+ggURL='https://drive.google.com/uc?export=download'  
+filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"  
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
+curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}" 
